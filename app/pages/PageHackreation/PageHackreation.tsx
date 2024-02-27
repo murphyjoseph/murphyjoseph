cc6d45541c5b9992d8611c2e13ae05499531b9fc
Joseph Murphy <murphyjoseph@users.noreply.github.com>
2024-02-27 11:24:25 -0600

    feat: add hackreation page

---
+++ app/pages/PageHackreation/PageHackreation.tsx
@@ -0,0 +1,14 @@
+import { NavigationMain } from "~/components/Navigation/NavigationMain";
+import { SectionHero } from "./SectionHero";
+
+export const PageHackreation = () => {
+
+  return (
+    <>
+      <NavigationMain />
+      <SectionHero
+        marginBottom={20}
+      />
+    </>
+  )
+}
